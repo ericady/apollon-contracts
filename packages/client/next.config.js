7d3c9608d7fa24a9b8872fad74edf3de0d0c773a
crismon-mint <chrissigrilus@gmail.com>
2023-08-09 04:55:17 +0200

    feat(client): add static export to next config

--- packages/client/next.config.js
+++ packages/client/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-}
+  // For now we are creating a SPA without performance feature of Next.
+  // This comes with some limitations: https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
+  output: 'export',
+  reactStrictMode: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
