c2f37675b1288e02611b0f60320f4349ec84a46e
wassaven <wassaven@gmail.com>
2023-10-05 01:12:50 +1000

    feat: disable contract sizer

--- packages/contracts/hardhat.config.js
+++ packages/contracts/hardhat.config.js
@@ -3,7 +3,7 @@ require('@nomiclabs/hardhat-ethers');
 require('@nomiclabs/hardhat-etherscan');
 require('solidity-coverage');
 require('hardhat-gas-reporter');
-require('hardhat-contract-sizer');
+// require('hardhat-contract-sizer');
 
 // todo tmp, ignores all contracts with _hardhatIgnore in the name
 const { subtask } = require('hardhat/config');
