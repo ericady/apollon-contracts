87e82e1f701e0d5395aa2b069373244b5d9be798
crismon-mint <chrissigrilus@gmail.com>
2023-10-31 18:33:36 +0100

    feat(contracts): migrate additional TroveManager tests

--- packages/contracts/test/TroveManagerTest.ts
+++ packages/contracts/test/TroveManagerTest.ts
@@ -73,7 +73,7 @@ describe.only('TroveManager', () => {
   });
 
   describe('TroveManager - in Recovery Mode', () => {
-    it.only('liquidate(): closes a Trove that has ICR < MCR', async () => {
+    it('liquidate(): closes a Trove that has ICR < MCR', async () => {
       const collToLiquidate = parseEther('300');
       // - GAS_COMPENSATION
       const collToDistribute = collToLiquidate - parseEther('200');
@@ -135,7 +135,7 @@ describe.only('TroveManager', () => {
       assert.lengthOf(aliceTroveColl, 0);
     });
 
-    it.only('liquidate(): decreases ActivePool collateral by liquidated amount but keeps the redemptionFee', async () => {
+    it('liquidate(): decreases ActivePool collateral by liquidated amount but keeps the redemptionFee', async () => {
       const initialColl = parseEther('1000');
       const collToLiquidate = parseEther('400');
 
@@ -179,6 +179,143 @@ describe.only('TroveManager', () => {
         storageActivePool_After,
         initialColl + bystandingColl + (collToLiquidate * redemptionFee) / parseEther('1')
       );
+    });
+
+    it('liquidate(): increases DefaultPool ETH and LUSD debt by correct amounts', async () => {
+      const collToLiquidate = parseEther('300');
+
+      // Add enough coll to not enter recovery mode
+      await borrowerOperations.testStoragePool_addValue(USDT, true, 0, parseEther('1000'));
+      // add value so that pool transfer can be made
+      await borrowerOperations.testStoragePool_addValue(STABLE, false, 0, collToLiquidate);
+
+      await openTrove({
+        from: alice,
+        contracts,
+        collToken: USDT,
+        collAmount: collToLiquidate,
+      });
+
+      await openTrove({
+        from: bob,
+        contracts,
+        collToken: USDT,
+        collAmount: parseEther('300'),
+      });
+
+      await borrowerOperations.testTroveManager_increaseTroveDebt(alice, [
+        { debtToken: STABLE.target, borrowingFee: parseEther('0.01'), netDebt: parseEther('300') },
+      ]);
+
+      const defaultPool_Before = await storagePool.getValue(USDT.target, true, 1);
+      assert.equal(defaultPool_Before, 0n);
+
+      // liquidate alice but spare bob
+      await troveManager.liquidate(alice);
+
+      const defaultPool_After = await storagePool.getValue(USDT.target, true, 1);
+      assert.equal(defaultPool_After, collToLiquidate - (collToLiquidate * redemptionFee) / parseEther('1'));
+    });
+
+    it("liquidate(): removes the Trove's stake from the total stakes", async () => {
+      const collToLiquidate = parseEther('300');
+
+      // Add enough coll to not enter recovery mode
+      await borrowerOperations.testStoragePool_addValue(USDT, true, 0, parseEther('1000'));
+      // add value so that pool transfer can be made
+      await borrowerOperations.testStoragePool_addValue(STABLE, false, 0, collToLiquidate);
+
+      await openTrove({
+        from: alice,
+        contracts,
+        collToken: USDT,
+        collAmount: collToLiquidate,
+      });
+
+      const bystandingColl = parseEther('300');
+      await openTrove({
+        from: bob,
+        contracts,
+        collToken: USDT,
+        collAmount: bystandingColl,
+      });
+
+      await borrowerOperations.testTroveManager_increaseTroveDebt(alice, [
+        { debtToken: STABLE.target, borrowingFee: parseEther('0.01'), netDebt: parseEther('300') },
+      ]);
+
+      const stakes_Before = await troveManager.totalStakes(USDT.target);
+      assert.equal(stakes_Before, collToLiquidate + bystandingColl);
+
+      // liquidate alice but spare bob
+      await troveManager.liquidate(alice);
+
+      const stakes_After = await troveManager.totalStakes(USDT.target);
+      assert.equal(stakes_After, bystandingColl);
+    });
+
+    it('liquidate(): Removes the correct trove from the TroveOwners array, and moves the last array element to the new empty slot', async () => {
+      const collToLiquidate = parseEther('300');
+
+      // Add enough coll to not enter recovery mode
+      await borrowerOperations.testStoragePool_addValue(USDT, true, 0, parseEther('1000'));
+      // add value so that pool transfer can be made
+      await borrowerOperations.testStoragePool_addValue(STABLE, false, 0, collToLiquidate);
+
+      await openTrove({
+        from: bob,
+        contracts,
+        collToken: USDT,
+        collAmount: parseEther('300'),
+      });
+
+      await openTrove({
+        from: alice,
+        contracts,
+        collToken: USDT,
+        collAmount: collToLiquidate,
+      });
+
+      await openTrove({
+        from: carol,
+        contracts,
+        collToken: USDT,
+        collAmount: parseEther('300'),
+      });
+
+      await borrowerOperations.testTroveManager_increaseTroveDebt(alice, [
+        { debtToken: STABLE.target, borrowingFee: parseEther('0.01'), netDebt: parseEther('300') },
+      ]);
+
+      const totalTroveOwners_before = await troveManager.getTroveOwnersCount();
+      assert.equal(totalTroveOwners_before, 3n);
+
+      const bob_arrayIndex = (await troveManager.Troves(bob))[1];
+      const alice_arrayIndex = (await troveManager.Troves(alice))[1];
+      const carol_arrayIndex = (await troveManager.Troves(carol))[1];
+
+      const trove_0 = await troveManager.TroveOwners(bob_arrayIndex);
+      const trove_1 = await troveManager.TroveOwners(alice_arrayIndex);
+      const trove_2 = await troveManager.TroveOwners(carol_arrayIndex);
+
+      assert.equal(trove_0, bob.address);
+      assert.equal(trove_1, alice.address);
+      assert.equal(trove_2, carol.address);
+
+      // liquidate alice but spare bob
+      await troveManager.liquidate(alice);
+
+      const totalTroveOwners_after = await troveManager.getTroveOwnersCount();
+      assert.equal(totalTroveOwners_after, 2n);
+
+      const bob_arrayIndex_after = (await troveManager.Troves(bob))[1];
+      const carol_arrayIndex_after = (await troveManager.Troves(carol))[1];
+
+      const trove_0_after = await troveManager.TroveOwners(bob_arrayIndex_after);
+      const trove_1_after = await troveManager.TroveOwners(carol_arrayIndex_after);
+
+      assert.equal(trove_0_after, bob.address);
+      assert.equal(trove_1_after, carol.address);
     });
   });
 });
