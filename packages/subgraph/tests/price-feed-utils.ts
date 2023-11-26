f5f8f482d70936ffff08a061b0826b4194a4331e
crismon-mint <chrissigrilus@gmail.com>
2023-11-26 14:04:11 +0100

    feat(subgraph): prettify

--- packages/subgraph/tests/price-feed-utils.ts
+++ packages/subgraph/tests/price-feed-utils.ts
@@ -1,91 +1,57 @@
-import { newMockEvent } from "matchstick-as"
-import { ethereum, Address, BigInt } from "@graphprotocol/graph-ts"
+import { Address, BigInt, ethereum } from '@graphprotocol/graph-ts';
+import { newMockEvent } from 'matchstick-as';
 import {
   LastGoodPriceUpdated,
   OwnershipTransferred,
   PriceFeedStatusChanged,
-  TokenPriceChanged
-} from "../generated/PriceFeed/PriceFeed"
+  TokenPriceChanged,
+} from '../generated/PriceFeed/PriceFeed';
 
-export function createLastGoodPriceUpdatedEvent(
-  _token: Address,
-  _lastGoodPrice: BigInt
-): LastGoodPriceUpdated {
-  let lastGoodPriceUpdatedEvent = changetype<LastGoodPriceUpdated>(
-    newMockEvent()
-  )
+export function createLastGoodPriceUpdatedEvent(_token: Address, _lastGoodPrice: BigInt): LastGoodPriceUpdated {
+  let lastGoodPriceUpdatedEvent = changetype<LastGoodPriceUpdated>(newMockEvent());
 
-  lastGoodPriceUpdatedEvent.parameters = new Array()
+  lastGoodPriceUpdatedEvent.parameters = new Array();
 
+  lastGoodPriceUpdatedEvent.parameters.push(new ethereum.EventParam('_token', ethereum.Value.fromAddress(_token)));
   lastGoodPriceUpdatedEvent.parameters.push(
-    new ethereum.EventParam("_token", ethereum.Value.fromAddress(_token))
-  )
-  lastGoodPriceUpdatedEvent.parameters.push(
-    new ethereum.EventParam(
-      "_lastGoodPrice",
-      ethereum.Value.fromUnsignedBigInt(_lastGoodPrice)
-    )
-  )
+    new ethereum.EventParam('_lastGoodPrice', ethereum.Value.fromUnsignedBigInt(_lastGoodPrice)),
+  );
 
-  return lastGoodPriceUpdatedEvent
+  return lastGoodPriceUpdatedEvent;
 }
 
-export function createOwnershipTransferredEvent(
-  previousOwner: Address,
-  newOwner: Address
-): OwnershipTransferred {
-  let ownershipTransferredEvent = changetype<OwnershipTransferred>(
-    newMockEvent()
-  )
+export function createOwnershipTransferredEvent(previousOwner: Address, newOwner: Address): OwnershipTransferred {
+  let ownershipTransferredEvent = changetype<OwnershipTransferred>(newMockEvent());
 
-  ownershipTransferredEvent.parameters = new Array()
+  ownershipTransferredEvent.parameters = new Array();
 
   ownershipTransferredEvent.parameters.push(
-    new ethereum.EventParam(
-      "previousOwner",
-      ethereum.Value.fromAddress(previousOwner)
-    )
-  )
-  ownershipTransferredEvent.parameters.push(
-    new ethereum.EventParam("newOwner", ethereum.Value.fromAddress(newOwner))
-  )
+    new ethereum.EventParam('previousOwner', ethereum.Value.fromAddress(previousOwner)),
+  );
+  ownershipTransferredEvent.parameters.push(new ethereum.EventParam('newOwner', ethereum.Value.fromAddress(newOwner)));
 
-  return ownershipTransferredEvent
+  return ownershipTransferredEvent;
 }
 
-export function createPriceFeedStatusChangedEvent(
-  _token: Address,
-  newStatus: i32
-): PriceFeedStatusChanged {
-  let priceFeedStatusChangedEvent = changetype<PriceFeedStatusChanged>(
-    newMockEvent()
-  )
+export function createPriceFeedStatusChangedEvent(_token: Address, newStatus: i32): PriceFeedStatusChanged {
+  let priceFeedStatusChangedEvent = changetype<PriceFeedStatusChanged>(newMockEvent());
 
-  priceFeedStatusChangedEvent.parameters = new Array()
+  priceFeedStatusChangedEvent.parameters = new Array();
 
+  priceFeedStatusChangedEvent.parameters.push(new ethereum.EventParam('_token', ethereum.Value.fromAddress(_token)));
   priceFeedStatusChangedEvent.parameters.push(
-    new ethereum.EventParam("_token", ethereum.Value.fromAddress(_token))
-  )
-  priceFeedStatusChangedEvent.parameters.push(
-    new ethereum.EventParam(
-      "newStatus",
-      ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(newStatus))
-    )
-  )
+    new ethereum.EventParam('newStatus', ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(newStatus))),
+  );
 
-  return priceFeedStatusChangedEvent
+  return priceFeedStatusChangedEvent;
 }
 
-export function createTokenPriceChangedEvent(
-  _token: Address
-): TokenPriceChanged {
-  let tokenPriceChangedEvent = changetype<TokenPriceChanged>(newMockEvent())
+export function createTokenPriceChangedEvent(_token: Address): TokenPriceChanged {
+  let tokenPriceChangedEvent = changetype<TokenPriceChanged>(newMockEvent());
 
-  tokenPriceChangedEvent.parameters = new Array()
+  tokenPriceChangedEvent.parameters = new Array();
 
-  tokenPriceChangedEvent.parameters.push(
-    new ethereum.EventParam("_token", ethereum.Value.fromAddress(_token))
-  )
+  tokenPriceChangedEvent.parameters.push(new ethereum.EventParam('_token', ethereum.Value.fromAddress(_token)));
 
-  return tokenPriceChangedEvent
+  return tokenPriceChangedEvent;
 }
