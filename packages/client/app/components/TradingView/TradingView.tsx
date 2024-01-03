cc14acf2a7283d9f5a5636aaa9ba85e3f8a6d295
crismon-mint <chrissigrilus@gmail.com>
2024-01-03 16:15:37 +0100

    feat(client): remove TradingView lightweight dependency

--- packages/client/app/components/TradingView/TradingView.tsx
+++ packages/client/app/components/TradingView/TradingView.tsx
@@ -1,94 +1,11 @@
 'use client';
 
 import { useTheme } from '@mui/material';
-import { ChartOptions, ColorType, CrosshairMode, LineStyle } from 'lightweight-charts';
 import { useEffect } from 'react';
 import '../../external/charting_library/charting_library';
 import { IChartingLibraryWidget } from '../../external/charting_library/charting_library';
 import { UDFCompatibleDatafeed } from '../../external/datafeeds/udf/dist/bundle.esm';
 import TradingViewHeader from './TradingViewHeader';
-
-const chartOptionsDark: Partial<ChartOptions> = {
-  grid: {
-    vertLines: {
-      style: LineStyle.Solid,
-      visible: true,
-      color: '#282531',
-    },
-    horzLines: {
-      style: LineStyle.Solid,
-      visible: true,
-      color: '#282531',
-    },
-  },
-  layout: {
-    textColor: '#B4B1BD',
-    fontFamily: 'Space Grotesk Variable',
-    fontSize: 11,
-    background: { type: ColorType.Solid, color: '#1e1b27' },
-  },
-  crosshair: {
-    horzLine: {
-      visible: true,
-      labelVisible: true,
-      color: '#e04a4a',
-      labelBackgroundColor: '#e04a4a',
-      style: LineStyle.Dashed,
-      width: 1,
-    },
-    vertLine: {
-      visible: true,
-      labelVisible: true,
-      color: '#e04a4a',
-      labelBackgroundColor: '#e04a4a',
-      style: LineStyle.Dashed,
-      width: 1,
-    },
-    mode: CrosshairMode.Normal,
-  },
-  autoSize: true,
-};
-
-const chartOptionsLight: Partial<ChartOptions> = {
-  grid: {
-    vertLines: {
-      style: LineStyle.Solid,
-      visible: true,
-      color: '#ECECEC',
-    },
-    horzLines: {
-      style: LineStyle.Solid,
-      visible: true,
-      color: '#ECECEC',
-    },
-  },
-  layout: {
-    textColor: '#696969',
-    fontFamily: 'Space Grotesk Variable',
-    fontSize: 11,
-    background: { type: ColorType.Solid, color: '#F8F8F8' },
-  },
-  crosshair: {
-    horzLine: {
-      visible: true,
-      labelVisible: true,
-      color: '#e04a4a',
-      labelBackgroundColor: '#e04a4a',
-      style: LineStyle.Dashed,
-      width: 1,
-    },
-    vertLine: {
-      visible: true,
-      labelVisible: true,
-      color: '#e04a4a',
-      labelBackgroundColor: '#e04a4a',
-      style: LineStyle.Dashed,
-      width: 1,
-    },
-    mode: CrosshairMode.Normal,
-  },
-  autoSize: true,
-};
 
 function TradingViewComponent() {
   const theme = useTheme();
