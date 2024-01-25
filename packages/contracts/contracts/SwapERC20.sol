b581dc14753c6dbe26be348053427bcf1ec67a89
Philipp Sambohl <philipp@sambohl.de>
2024-01-25 11:03:47 +0000

    removed old swap erc permit functions

--- packages/contracts/contracts/SwapERC20.sol
+++ packages/contracts/contracts/SwapERC20.sol
@@ -10,29 +10,6 @@ contract SwapERC20 is ISwapERC20 {
   uint8 public constant decimals = 18;
   uint public totalSupply;
   mapping(address => uint) public balanceOf;
-  mapping(address => mapping(address => uint)) public allowance;
-
-  bytes32 public DOMAIN_SEPARATOR;
-  // keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
-  bytes32 public constant PERMIT_TYPEHASH = 0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9;
-  mapping(address => uint) public nonces;
-
-  constructor() {
-    uint chainId;
-    assembly {
-      chainId := chainid()
-    }
-
-    DOMAIN_SEPARATOR = keccak256(
-      abi.encode(
-        keccak256('EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)'),
-        keccak256(bytes(name)),
-        keccak256(bytes('1')),
-        chainId,
-        address(this)
-      )
-    );
-  }
 
   function _mint(address to, uint value) internal {
     totalSupply += value;
