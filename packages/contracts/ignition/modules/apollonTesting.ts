6c5866040e115a9f7d9ef5cb3243a67f781d9332
Philipp Sambohl <philipp@sambohl.de>
2024-02-13 12:44:11 +0000

    seeded test pools

--- packages/contracts/ignition/modules/apollonTesting.ts
+++ packages/contracts/ignition/modules/apollonTesting.ts
@@ -1,5 +1,5 @@
 import { buildModule } from '@nomicfoundation/hardhat-ignition/modules';
-import { parseUnits } from 'ethers';
+import { MaxUint256, parseUnits, ZeroAddress } from 'ethers';
 
 export default buildModule('ApollonTesting', m => {
   const deadline = m.getParameter('deadline');
@@ -216,49 +216,94 @@ export default buildModule('ApollonTesting', m => {
     after: [contracts.GOV, tokenLink],
   });
 
-  // todo
-  // // setup swap pools
-  // const stableMint = m.call(contracts.STABLE, 'unprotectedMint', [m.getAccount(0), 100000], { id: 'mintStable' });
-  // const stockMint = m.call(contracts.STOCK, 'unprotectedMint', [m.getAccount(0), 100000], { id: 'mintStock' });
-  // const btcMint = m.call(contracts.BTC, 'unprotectedMint', [m.getAccount(0), 10], { id: 'mintBTC' });
-  // const usdtMint = m.call(contracts.USDT, 'unprotectedMint', [m.getAccount(0), 100000], { id: 'mintUSDT' });
-  //
-  // const stableApprove = m.call(contracts.STABLE, 'approve', [contracts.swapOperations, MaxUint256], {
-  //   id: 'approveStable',
-  //   after: [stableMint],
-  // });
-  // const stockApprove = m.call(contracts.STOCK, 'approve', [contracts.swapOperations, MaxUint256], {
-  //   id: 'approveStock',
-  //   after: [stockMint],
-  // });
-  // const btcApprove = m.call(contracts.BTC, 'approve', [contracts.swapOperations, MaxUint256], {
-  //   id: 'approveBTC',
-  //   after: [btcMint],
-  // });
-  // const usdtApprove = m.call(contracts.USDT, 'approve', [contracts.swapOperations, MaxUint256], {
-  //   id: 'approveUSDT',
-  //   after: [usdtMint],
-  // });
-  //
-  // // STABLE - BTC
-  // const stableBTCPair = m.call(contracts.swapOperations, 'createPair', [contracts.BTC, contracts.STABLE], {
-  //   id: 'createPairBTCSTABLE',
-  // });
-  // m.call(
-  //   contracts.swapOperations,
-  //   'addLiquidity',
-  //   [
-  //     contracts.BTC,
-  //     contracts.STABLE,
-  //     1,
-  //     10000,
-  //     0,
-  //     0,
-  //     { upperHint: ZeroAddress, lowerHint: ZeroAddress, maxFeePercentage: 0 },
-  //     99999999999, // todo dynamic deadline (await getLatestBlockTimestamp()) + 100;
-  //   ],
-  //   { id: 'addLiquidityBTCSTABLE', after: [stableBTCPair, btcApprove, stableApprove] }
-  // );
+  // setup swap pools
+  const stableMint = m.call(contracts.STABLE, 'unprotectedMint', [m.getAccount(0), parseUnits('40000')], {
+    id: 'mintStable',
+  });
+  const stockMint = m.call(contracts.STOCK, 'unprotectedMint', [m.getAccount(0), parseUnits('65')], {
+    id: 'mintStock',
+  });
+  const btcMint = m.call(contracts.BTC, 'unprotectedMint', [m.getAccount(0), parseUnits('1', 9)], { id: 'mintBTC' });
+  const usdtMint = m.call(contracts.USDT, 'unprotectedMint', [m.getAccount(0), parseUnits('10000')], {
+    id: 'mintUSDT',
+  });
+
+  const stableApprove = m.call(contracts.STABLE, 'approve', [contracts.swapOperations, MaxUint256], {
+    id: 'approveStable',
+    after: [stableMint],
+  });
+  const stockApprove = m.call(contracts.STOCK, 'approve', [contracts.swapOperations, MaxUint256], {
+    id: 'approveStock',
+    after: [stockMint],
+  });
+  const btcApprove = m.call(contracts.BTC, 'approve', [contracts.swapOperations, MaxUint256], {
+    id: 'approveBTC',
+    after: [btcMint],
+  });
+  const usdtApprove = m.call(contracts.USDT, 'approve', [contracts.swapOperations, MaxUint256], {
+    id: 'approveUSDT',
+    after: [usdtMint],
+  });
+
+  // STABLE - BTC
+  const stableBTCPair = m.call(contracts.swapOperations, 'createPair', [contracts.BTC, contracts.STABLE], {
+    id: 'createPairBTC',
+  });
+  m.call(
+    contracts.swapOperations,
+    'addLiquidity',
+    [
+      contracts.BTC,
+      contracts.STABLE,
+      parseUnits('1', 9),
+      parseUnits('20000'),
+      0,
+      0,
+      { upperHint: ZeroAddress, lowerHint: ZeroAddress, maxFeePercentage: 0 },
+      deadline,
+    ],
+    { id: 'addLiquidityBTC', after: [stableBTCPair, btcApprove, stableApprove] }
+  );
+
+  // STABLE - USDT
+  const stableUSDTPair = m.call(contracts.swapOperations, 'createPair', [contracts.USDT, contracts.STABLE], {
+    id: 'createPairUSDT',
+  });
+  m.call(
+    contracts.swapOperations,
+    'addLiquidity',
+    [
+      contracts.USDT,
+      contracts.STABLE,
+      parseUnits('10000'),
+      parseUnits('10000'),
+      0,
+      0,
+      { upperHint: ZeroAddress, lowerHint: ZeroAddress, maxFeePercentage: 0 },
+      deadline,
+    ],
+    { id: 'addLiquidityUSDT', after: [stableUSDTPair, usdtApprove, stableApprove] }
+  );
+
+  // STABLE - STOCK
+  const stableStockPair = m.call(contracts.swapOperations, 'createPair', [contracts.STOCK, contracts.STABLE], {
+    id: 'createPairSTOCK',
+  });
+  m.call(
+    contracts.swapOperations,
+    'addLiquidity',
+    [
+      contracts.STOCK,
+      contracts.STABLE,
+      parseUnits('65'),
+      parseUnits('10000'),
+      0,
+      0,
+      { upperHint: ZeroAddress, lowerHint: ZeroAddress, maxFeePercentage: 0 },
+      deadline,
+    ],
+    { id: 'addLiquiditySTOCK', after: [stableStockPair, stockApprove, stableApprove] }
+  );
 
   return contracts;
 });
